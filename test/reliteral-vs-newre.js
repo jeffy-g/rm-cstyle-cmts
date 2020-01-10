a1bfe9a5a884261e66cd2e059b150c8c86627174
jeffy-g <hirotom1107@gmail.com>
2020-01-10 13:43:38 +0900

    🔀 moved test script

--- test/reliteral-vs-newre.js
+++ test/reliteral-vs-newre.js
@@ -7,7 +7,7 @@
 // In this issue, I wrote a simple test code and executed it in order to renew awareness about this concern.
 //
 const OUTER_REPEAT = 4;
-const REPEAT = 10000000;
+const REPEAT = 1000000;
 function regexLiteralVsNewRegex(innerRepeat = REPEAT) {
 
     console.log("test case literal x %d", innerRepeat);
@@ -15,7 +15,7 @@ function regexLiteralVsNewRegex(innerRepeat = REPEAT) {
         let i = innerRepeat;
         console.time("test");
         while (i--) {
-            /\n\s+(?=\n)|\s+(?=\n)|`|"(?:[^\\"]+|\\[\s\S])*"|'(?:[^\\']+|\\[\s\S])*'|\//g;
+            /\n\s+(?=\n)|\s+(?=\n)|`|"(?:[^\\"]+|\\[\s\S])*"|'(?:[^\\']+|\\[\s\S])*'|\//g.test("'text'");
         }
         console.timeEnd("test");
     }
@@ -25,7 +25,7 @@ function regexLiteralVsNewRegex(innerRepeat = REPEAT) {
         let i = innerRepeat;
         console.time("test");
         while (i--) {
-            new RegExp(`\\n\\s+(?=\\n)|\\s+(?=\\n)|\`|"(?:[^\\\\"]+|\\\\[\\s\\S])*"|'(?:[^\\\\']+|\\\\[\\s\\S])*'|\\/`, "g");
+            new RegExp(`\\n\\s+(?=\\n)|\\s+(?=\\n)|\`|"(?:[^\\\\"]+|\\\\[\\s\\S])*"|'(?:[^\\\\']+|\\\\[\\s\\S])*'|\\/`, "g").test("'text'");
         }
         console.timeEnd("test");
     }
@@ -33,27 +33,26 @@ function regexLiteralVsNewRegex(innerRepeat = REPEAT) {
 /*
 [node v13.4.0]
 > regexLiteralVsNewRegex();
-test case literal x 10000000
-test: 11.999ms
-test: 12.769ms
-test: 8.85ms
-test: 8.06ms
-test case new regex instance x 10000000
-test: 4.712s
-test: 4.814s
-test: 4.731s
-test: 4.698s
+test case literal x 1000000
+test: 47.177ms
+test: 40.623ms
+test: 37.012ms
+test: 36.862ms
+test case new regex instance x 1000000
+test: 358.043ms
+test: 358.057ms
+test: 357.271ms
+test: 355.504ms
 
-[Chromium: 79.0.3945.88（Official Build） (64-bit)]
+[Google Chrome: 79.0.3945.117（Official Build） （64-bit）]
 regexLiteralVsNewRegex();
-test case literal x 10000000
-test: 12.472900390625ms
-test: 11.4501953125ms
-test: 6.94482421875ms
-test: 6.94384765625ms
-test case new regex instance x 10000000
-test: 3828.946044921875ms
-test: 3827.702880859375ms
-test: 3817.5830078125ms
-test: 3899.433837890625ms
+test: 55.455078125ms
+test: 43.53125ms
+test: 39.3779296875ms
+test: 38.83984375ms
+test case new regex instance x 1000000
+test: 211.5498046875ms
+test: 212.908203125ms
+test: 209.93212890625ms
+test: 210.4599609375ms
 */
