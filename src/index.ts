21fe89b9dddb5da4a89eba047d3094929dc3d7cb
jeffy-g <hirotom1107@gmail.com>
2025-01-29 04:16:34 +0900

    v3.3.19

--- src/index.ts
+++ src/index.ts
@@ -118,7 +118,7 @@ const rmc = /** @type {IRemoveCStyleComments} */( emitMainFunction(apply) ) as I
 Object.defineProperties(rmc, {
     version: {
         // `npm run patch:tag` replaces version string
-        value: "v3.3.18",
+        value: "v3.3.19",
         enumerable: true
     },
     walk: {
