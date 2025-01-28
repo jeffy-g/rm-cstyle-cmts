1ec740d56e64fbcaf5823645ab2aba15b6d2040a
jeffy-g <hirotom1107@gmail.com>
2025-01-28 21:14:41 +0900

    v3.3.16

--- src/index.ts
+++ src/index.ts
@@ -118,7 +118,7 @@ const rmc = /** @type {IRemoveCStyleComments} */( emitMainFunction(apply) ) as I
 Object.defineProperties(rmc, {
     version: {
         // `npm run patch:tag` replaces version string
-        value: "v3.3.15",
+        value: "v3.3.16",
         enumerable: true
     },
     walk: {
